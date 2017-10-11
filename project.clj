d5df5ccda358f18c7958dfd1f86c3eb380689907
Matthew Huebert <no-email@gmail.com>
2017-10-11 17:47:41 +0200

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject mhuebert/keys-test "0.1.0-SNAPSHOT"
-  :dependencies [[re-view "0.3.32-SNAPSHOT"]
-                 [thheller/shadow-cljs "2.0.8"]]
+(defproject mhuebert/keys-test "0.1.0"
+  :dependencies [[thheller/shadow-cljs "2.0.8"]
+                 [re-view "0.3.33-SNAPSHOT"]
+                 [re-view-routing "0.1.3"]]
   :source-paths ["src"])
